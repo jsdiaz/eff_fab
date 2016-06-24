22976a41f4cdb70455e755b403067fbe73e1ee30
fab role account <fab-dev@cyber.law.harvard.edu>
2016-06-24 13:49:30 -0400

    rake db:seed now works, was coughing on username number increment implementation

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create!(name: 'Emanuel', city: cities.first)
 
 User.delete_all
 Fab.delete_all
@@ -13,20 +13,20 @@ Note.delete_all
 def create_teams
   Team.delete_all
 
-  Team.create(name: "Activism", weight: 100)
-  Team.create(name: "Press/ Graphics", weight: 110)
-  Team.create(name: "Executive", weight: 190)
-  Team.create(name: "Executive Support", weight: 180)
-  Team.create(name: "Operations", weight: 170)
-  Team.create(name: "Development", weight: 150)
-  Team.create(name: "Finance/ HR", weight: 160)
-  Team.create(name: "Tech Ops", weight: 140)
-  Team.create(name: "Webdev", weight: 130)
-  Team.create(name: "Tech Projects", weight: 120)
-  Team.create(name: "Civil Liberties", weight: 90)
-  Team.create(name: "Intellectual Property", weight: 80)
-  Team.create(name: "International", weight: 70)
-  Team.create(name: "Other", weight: 200)
+  Team.create!(name: "Activism", weight: 100)
+  Team.create!(name: "Press/ Graphics", weight: 110)
+  Team.create!(name: "Executive", weight: 190)
+  Team.create!(name: "Executive Support", weight: 180)
+  Team.create!(name: "Operations", weight: 170)
+  Team.create!(name: "Development", weight: 150)
+  Team.create!(name: "Finance/ HR", weight: 160)
+  Team.create!(name: "Tech Ops", weight: 140)
+  Team.create!(name: "Webdev", weight: 130)
+  Team.create!(name: "Tech Projects", weight: 120)
+  Team.create!(name: "Civil Liberties", weight: 90)
+  Team.create!(name: "Intellectual Property", weight: 80)
+  Team.create!(name: "International", weight: 70)
+  Team.create!(name: "Other", weight: 200)
 end
 
 def rand_team_id
@@ -38,26 +38,26 @@ def build_fabs(u)
   this_monday = DateTime.now.in_time_zone - DateTime.now.in_time_zone.wday + 1.day
   last_monday = this_monday - 7.days
 
-  f = u.fabs.create(period: last_monday)
+  f = u.fabs.create!(period: last_monday)
   f.notes.delete_all
-  3.times { f.notes.create(body: "I did a thing", forward: false) }
-  3.times { f.notes.create(body: "I will do a thing", forward: true) }
+  3.times { f.notes.create!(body: "I did a thing", forward: false) }
+  3.times { f.notes.create!(body: "I will do a thing", forward: true) }
 
-  f = u.fabs.create(period: this_monday)
+  f = u.fabs.create!(period: this_monday)
   f.notes.delete_all
-  3.times { f.notes.create(body: "I was SUPER", forward: false) }
-  3.times { f.notes.create(body: "I will be more super", forward: true) }
+  3.times { f.notes.create!(body: "I was SUPER", forward: false) }
+  3.times { f.notes.create!(body: "I will be more super", forward: true) }
 end
 
 create_teams
 
 i = 0
-20.times { build_fabs(User.create(name: "AutoGenned User", email: "user#{i++}@example.com", team_id: rand_team_id)) }
+20.times {|i| build_fabs(User.create!(name: "AutoGenned User", email: "user#{i}@example.com", team_id: rand_team_id, password: "password#{i}")) }
 
 u = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << u.email
 
-u.name = "Hugh D\`Andrade"
+u.name = "Joe Schmoe"
 u.save
 
 build_fabs(u)
